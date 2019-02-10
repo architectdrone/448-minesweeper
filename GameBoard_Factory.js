73cbba3a79443f4e1d3bd821c36bde74b5df1037
Adam Wallacessh-keygen -t rsa -b 4096 -C awallace689@gmail.com <awallace689@gmail.com>
2019-02-10 11:47:39 -0600

    added create_image_element and key object, invalid output

--- GameBoard_Factory.js
+++ GameBoard_Factory.js
@@ -1,3 +1,15 @@
+let key = {
+    bomb: create_image_element('img/pAK004-boom.png')
+};
+
+function create_image_element(path){
+    let elem_img = document.createElement('img');
+    elem_img.setAttribute('src', path);
+    elem_img.setAttribute('id', 'img');
+
+    return elem_img;
+}
+
 function build_grid(row, col) {
     let array2D = new Array(row);
     for (let i = 0; i < array2D.length; i++) {
@@ -17,17 +29,16 @@ function place_grid(html_table) {
 function build_html_grid(grid){
     let html_table = document.createElement("table");
 
-    for(let i = 0; i < grid.length; i++) {
+    for (let i = 0; i < grid.length; i++) {
         let row = document.createElement('tr');
         html_table.appendChild(row);
 
-        for(let j = 0; j < grid[i].length; j++) {
+        for (let j = 0; j < grid[i].length; j++) {
             let data = document.createElement('td');
-            let text = j.toString();
+            let img = key.bomb;
 
-            data.appendChild(document.createTextNode(text));
-            data.style.border = '1px solid black';
-            html_table.childNodes[i].appendChild(data);
+            data.appendChild(img);
+            row.appendChild(data);
         }
     }
     return html_table;
@@ -36,9 +47,9 @@ function build_html_grid(grid){
 function style_grid(html_table) {
     html_table.style.borderCollapse = "collapse";
 
-    for(let i = 0; i < html_table.childNodes.length; i++) {
+    for (let i = 0; i < html_table.childNodes.length; i++) {
         let row = html_table.childNodes[i];
-        for(let j = 0; j < row.childNodes.length; j++) {
+        for (let j = 0; j < row.childNodes.length; j++) {
             row.childNodes[j].style.border = "1px solid black";
         }
     }
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 448-minesweeper
 EECS 448 Group Project #1 - Miller Bath, Adam Wallace, Alex Wittman, Allen Lu, Teddy Kahwaji
 Started on 2/7/19
+
+All images sourced from 'openclipart' at https://openclipart.org/.
+Bomb img: https://openclipart.org/detail/191907/bomb
