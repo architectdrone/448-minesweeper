56dbbf92ee402c3c06e0359641b5209a1637edf4
Adam Wallacessh-keygen -t rsa -b 4096 -C awallace689@gmail.com <awallace689@gmail.com>
2019-02-10 12:49:19 -0600

    fixed output, styling, and cleaned up styles.css

--- GameBoard_Factory.js
+++ GameBoard_Factory.js
@@ -1,11 +1,13 @@
-let key = {
-    bomb: create_image_element('img/pAK004-boom.png')
-};
+class Key {
+    constructor() {
+        this.bomb = create_image_element('img/pAK004-boom.png');
+        this.concealed = create_image_element('img/square.png');
+    }
+}
 
 function create_image_element(path){
     let elem_img = document.createElement('img');
     elem_img.setAttribute('src', path);
-    elem_img.setAttribute('id', 'img');
 
     return elem_img;
 }
@@ -34,8 +36,9 @@ function build_html_grid(grid){
         html_table.appendChild(row);
 
         for (let j = 0; j < grid[i].length; j++) {
+            let key_list = new Key();
             let data = document.createElement('td');
-            let img = key.bomb;
+            let img = key_list.bomb;
 
             data.appendChild(img);
             row.appendChild(data);
@@ -45,12 +48,20 @@ function build_html_grid(grid){
 }
 
 function style_grid(html_table) {
+    //style <table>
+    html_table.style.tableLayout = "fixed";
     html_table.style.borderCollapse = "collapse";
 
     for (let i = 0; i < html_table.childNodes.length; i++) {
         let row = html_table.childNodes[i];
         for (let j = 0; j < row.childNodes.length; j++) {
+            //style <td>
             row.childNodes[j].style.border = "1px solid black";
+            row.childNodes[j].style.padding = "0";
+            //style <img> in <td><img><td/>
+            row.childNodes[j].childNodes[0].style.display = "block";
+            row.childNodes[j].childNodes[0].style.width = "25px";
+            row.childNodes[j].childNodes[0].style.height = "25px";
         }
     }
     return html_table;
