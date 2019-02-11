9b39356f4bef55fa96c60da31eac8624034f01f0
Teddy Kahwaji <teddykahwaji86@gmail.com>
2019-02-11 09:37:35 -0600

    Updated loop to ensure bombs don't get placed in same spot twice

--- FillAlgo.js
+++ FillAlgo.js
@@ -3,8 +3,9 @@
 /*
 Function Allows for the creation of a
 two dimensional array
-
  */
+
+
 function Array2DCreator(col, row) {
     let array2D = new Array(row);      //define outer array
     for (let i = 0; i < array2D.length; i++)
@@ -32,10 +33,14 @@ function return_2DBoard(row,col,bombNum)
     {
         bombPlacement1 = (Math.floor(Math.random() * row));
         bombPlacement2 = (Math.floor(Math.random() * col));
-        if(key2DArray[bombPlacement1][bombPlacement2] != 'b')
+        while(key2DArray[bombPlacement1][bombPlacement2] == 'b')
         {
-            key2DArray[bombPlacement1][bombPlacement2] = 'b';
+            bombPlacement1 = (Math.floor(Math.random() * row));
+            bombPlacement2 = (Math.floor(Math.random() * col));
+
+
         }
+        key2DArray[bombPlacement1][bombPlacement2] = 'b';
 
     }
     for (let k =0; k<row; k++)
