603bd79719c33820de556f68689c03bd782bd740
Miller Bath <millerzbath@gmail.com>
2019-02-13 16:32:36 -0600

    Fixed scrolling to the sides, added event to stop html reload, fixed button, and added comments for possible formatting down the road.

--- js/TopLevel.js
+++ js/TopLevel.js
@@ -5,19 +5,26 @@ function maxCheck()
   let BombInput = document.getElementById("uB");
 
   BombInput.max = ((RowInput.value*ColInput.value)-1);
-
-  //Trying to account for the resizing of the bomb input field
-  //resizing does not work
-  //BombInput.size = 100;
-  //BombInput.width = 500;
-
-
 }
+
 
 function CreateBoard(NumRows, NumCols, NumBombs)
 {
+
     //Uncomment the next line when the board creation functions are complete.
     //Fill_Algo(NumRows, NumCols, NumBombs);
 
-
 }
+
+//gives javascript the reins for not letting page reload
+form = document.getElementById('msform');
+form.addEventListener('submit',(event) =>{
+  console.log("Hello world");
+  event.preventDefault();
+});
+
+
+//fix positioning on form and header
+//fix position on wizard and ok buttons
+//fix size of ok button/wizard
+//maybe look into other computers compatability with the website displaying stuff
