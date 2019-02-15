efdf87e51201ae8b84684dc82299f76e06e5bf7b
wittmanalex <wittmanalex@gmail.com>
2019-02-15 15:44:22 -0600

    Refactored recursive function into clearer functions.

--- js/Sketch.js
+++ js/Sketch.js
@@ -1,36 +1,14 @@
-// function setup(height, width, numBombs) {
-//   /*let width=100;
-//   let height=100;
-//   cols=Math.floor(width/w);
-//   rows=Math.floor(height/w);*/
-//   cols = width;
-//   rows = height;
-//   grid=Array2DCreator(cols, rows);
-//   for(let i=0; i<rows; i++)
-//   {
-//     for(let j=0; j<cols; j++)
-//     {
-//         grid[i][j]=new Square(i, j, w);
-//       }
-//     }
-//     Placebomb(grid, rows, cols, numBombs);
-//
-//      for(let i=0; i<rows; i++)
-//      {
-//        for(let j=0; j<cols; j++)
-//        {
-//            grid[i][j].checkNeigbor();
-//          }
-//        }
-//       return grid;
-//   }
+let grid;
 
-function setup(rows, cols, numBombs){
-  let grid = Array2DCreator(rows, cols);
+function setup(rows, cols, numBombs)
+{
+  grid = Array2DCreator(rows, cols);
   PlaceBombs(grid, numBombs);
 
-  for (let j = 0; j < grid.length; j++) {
-    for (let k = 0; k < grid[j].length; k++){
+  for (let j = 0; j < grid.length; j++)
+  {
+    for (let k = 0; k < grid[j].length; k++)
+    {
       grid[j][k].checkNeighbor(grid, rows, cols);
     }
   }
@@ -43,28 +21,32 @@ function PlaceBombs(grid, numBombs)
   console.log(grid);
   let x = (Math.floor(Math.random() * grid.length));
   let y = (Math.floor(Math.random() * grid[x].length));
-    for(let i=0; i<numBombs; i++){
+  for(let i=0; i<numBombs; i++)
+  {
 
-      while(grid[x][y].bomb == true){
-        x = (Math.floor(Math.random() * grid.length));
-        y = (Math.floor(Math.random() * grid[x].length));
-      }
-      grid[x][y].bomb = true;
+    while(grid[x][y].bomb == true)
+    {
+      x = (Math.floor(Math.random() * grid.length));
+      y = (Math.floor(Math.random() * grid[x].length));
+    }
+    grid[x][y].bomb = true;
   }
 }
 
 
-function Array2DCreator(row, col) {
-    let array2D = new Array(row);
-    console.log(typeof(row));
-    for (let i = 0; i < array2D.length; i++){
-        array2D[i] = new Array(col);
-
-        for (let j = 0; j < array2D[i].length; j++){
-          array2D[i][j] = new Square(i, j);
-        }
-    }
-    return(array2D);
+function Array2DCreator(row, col)
+{
+  let array2D = new Array(row);
+  console.log(typeof(row));
+  for (let i = 0; i < array2D.length; i++)
+  {
+      array2D[i] = new Array(col);
+      for (let j = 0; j < array2D[i].length; j++)
+      {
+        array2D[i][j] = new Square(i, j);
+      }
+  }
+  return(array2D);
 }
 
 
@@ -72,7 +54,6 @@ function Square(i, j)
 {
   this.i=i;
   this.j=j;
-
   this.bombnearby=0;
   this.bomb = false;
   this.revealed=false;
@@ -80,149 +61,143 @@ function Square(i, j)
   this.key=-1;
 }
 
-Square.prototype.recreveal = function(grid, rows, cols){
+//Returns true if the given position is within the board's bounds.
+function IsWithinBoard(PosX, PosY)
+{
+  return PosX < grid.length && PosY < grid[0].length && PosX >= 0 && PosY >= 0;
+}
+
+//Reveals the tile at the given position and recursively calls the tiles around it.
+function RevealTile(PosX, PosY)
+{
+  if(!grid[PosX][PosY].revealed)
+  {
+    grid[PosX][PosY].recreveal();
+  }
+}
+
+//Checks if a position is within the board's bounds, then reveals that tile.
+function CheckTile(PosX, PosY)
+{
+  if(IsWithinBoard(PosX, PosY))
+  {
+    RevealTile(PosX, PosY)
+  }
+}
+
+//Checks the tile up and to the left of the given tile.
+Square.prototype.TopLeft = function(func)
+{
+  return func(this.i - 1, this.j - 1);
+}
+
+//Checks the tile directly above the given tile.
+Square.prototype.TopMiddle = function(func)
+{
+  return func(this.i, this.j - 1);
+}
+
+//Checks the tile up and to the right of the given tile.
+Square.prototype.TopRight = function(func)
+{
+  return func(this.i + 1, this.j - 1);
+}
+
+//Checks the tile directly to the left of the given tile.
+Square.prototype.Left = function(func)
+{
+  return func(this.i - 1, this.j);
+}
+
+//Checks the tile directly to the right of the given tile.
+Square.prototype.Right = function(func)
+{
+  return func(this.i + 1, this.j);
+}
+
+//Checks the tile down and to the left of the given tile.
+Square.prototype.BottomLeft = function(func)
+{
+  return func(this.i - 1, this.j + 1);
+}
+
+//Checks the tile directly below the given tile.
+Square.prototype.BottomMiddle = function(func)
+{
+  return func(this.i, this.j + 1);
+}
+
+//Checks the tile down and to the right of the given tile.
+Square.prototype.BottomRight = function(func)
+{
+  return func(this.i + 1, this.j + 1);
+}
+
+//Recursively reveals the tiles by checking the contents, then the surrounding tiles.
+Square.prototype.recreveal = function(){
   if(this.bomb === false)
   {
     this.revealed=true;
+
+    //If the tile clicked is blank then recurse.
     if(this.bombnearby==0)
     {
-        this.key=0;
-        if(this.i+1<rows && this.j+1<cols && this.i+1>=0 && this.j+1>=0)
-      {
-        if(!grid[this.i+1][this.j+1].revealed)
-      {
-        grid[this.i+1][this.j+1].recreveal(grid, rows, cols);
-        }
-      }
-      if(this.i-1<rows && this.j-1<cols && this.i-1>=0 && this.j-1>=0)
-      {
-        if(!grid[this.i-1][this.j-1].revealed)
-        {
-          grid[this.i-1][this.j-1].recreveal(grid, rows, cols);
-        }
-      }
-      if(this.i+1<rows && this.j-1<cols && this.i+1>=0 && this.j-1>=0)
-      {
-        if(!grid[this.i+1][this.j-1].revealed)
-        {
-          grid[this.i+1][this.j-1].recreveal(grid, rows, cols);
-        }
-      }
-      if(this.i-1<rows && this.j+1<cols && this.i-1>=0 && this.j+1>=0)
-      {
-        if(!grid[this.i-1][this.j+1].revealed)
-       {
-         grid[this.i-1][this.j+1].recreveal(grid, rows, cols);
-       }
-      }
-      if(this.j+1<cols)
-      {
-        if(!grid[this.i][this.j+1].revealed)
-        {
-          grid[this.i][this.j+1].recreveal(grid, rows, cols);
-        }
-      }
-      if(this.j-1>=0)
-      {
-        if(!grid[this.i][this.j-1].revealed)
-       {
-        grid[this.i][this.j-1].recreveal(grid, rows, cols);
-       }
-      }
-      if(this.i+1<rows)
-      {
-        if(!grid[this.i+1][this.j].revealed)
-       {
-         grid[this.i+1][this.j].recreveal(grid, rows, cols);
-       }
-      }
-      if(this.i-1>=0)
-      {
-        if(!grid[this.i-1][this.j].revealed)
-        {
-          grid[this.i-1][this.j].recreveal(grid, rows, cols);
-        }
-      }
+      //Set the image to the blank image.
+      this.key=0;
+
+      //Checks each of the 8 positions around the tile and reveals the non bombs.
+      this.TopLeft(CheckTile);
+      this.TopMiddle(CheckTile);
+      this.TopRight(CheckTile);
+      this.Left(CheckTile);
+      this.Right(CheckTile);
+      this.BottomLeft(CheckTile);
+      this.BottomMiddle(CheckTile);
+      this.BottomRight(CheckTile);
+
     }
     else
-    {
+    { //Otherwise the tile clicked is a number, so reveal the number.
       this.revealed = true;
       this.key = this.bombnearby;
     }
   }
   else
   {
+    //Call lose function because user clicked a bomb.
     this.revealed = true;
   }
 }
 
-
-Square.prototype.contains = function(x,y){
-  if(x>this.x && x<this.x+this.w && y>this.y && y<this.y+this.w)
+function IsBomb(PosX, PosY)
+{
+  if(IsWithinBoard(PosX, PosY))
   {
-    return(true);
+    if(grid[PosX][PosY].bomb==true)
+    {
+      return 1;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+  else
+  {
+    return 0;
   }
 }
 
+//Calculates the values around the bombs to display.
 Square.prototype.checkNeighbor = function(grid, rows, cols){
-    let countbombs=0;
-if(this.i+1<rows && this.j+1<cols && this.i+1>=0 && this.j+1>=0)
-{
-  if(grid[this.i+1][this.j+1].bomb==true)
-  {
-    countbombs=countbombs+1;
-  }
-}
-  if(this.i-1<rows && this.j-1<cols && this.i-1>=0 && this.j-1>=0)
-{
-  if(grid[this.i-1][this.j-1].bomb==true)
-  {
-    countbombs=countbombs+1;
-  }
-}
-if(this.i+1<rows && this.j-1<cols && this.i+1>=0 && this.j-1>=0)
-{
-  if(grid[this.i+1][this.j-1].bomb==true)
-  {
-    countbombs=countbombs+1;
-  }
-}
-  if(this.i-1<rows && this.j+1<cols && this.i-1>=0 && this.j+1>=0)
-{
-  if(grid[this.i-1][this.j+1].bomb==true)
-  {
-    countbombs=countbombs+1;
-  }
-}
-if(this.j+1<cols)
-{
-  if(grid[this.i][this.j+1].bomb==true)
-  {
-    countbombs=countbombs+1;
-  }
-}
-if(this.j-1>=0)
-{
-  if(grid[this.i][this.j-1].bomb==true)
-  {
-    countbombs=countbombs+1;
-  }
-}
-if(this.i+1<rows)
-{
-  if(grid[this.i+1][this.j].bomb==true)
-  {
-    countbombs=countbombs+1;
-  }
-}
-if(this.i-1>=0)
-{
-  if(grid[this.i-1][this.j].bomb==true)
-  {
-    countbombs=countbombs+1;
-  }
-}
- this.bombnearby= countbombs;
+  this.bombnearby += this.TopLeft(IsBomb);
+  this.bombnearby += this.TopMiddle(IsBomb);
+  this.bombnearby += this.TopRight(IsBomb);
+  this.bombnearby += this.Left(IsBomb);
+  this.bombnearby += this.Right(IsBomb);
+  this.bombnearby += this.BottomLeft(IsBomb);
+  this.bombnearby += this.BottomMiddle(IsBomb);
+  this.bombnearby += this.BottomRight(IsBomb);
 }
 
 Square.prototype.placebommb = function(){
