308cbeeaaa2b5e5d1e37cf1773e6f2933fef48a2
wittmanalex <wittmanalex@gmail.com>
2019-02-16 16:08:00 -0600

    Fixed the weird input issues. Now it works just as we had hoped.

--- js/TopLevel.js
+++ js/TopLevel.js
@@ -7,32 +7,53 @@ function maxCheck()
   BombInput.max = ((RowInput.value*ColInput.value)-1);
 }
 
+function GoodInputs()
+{
+  let RowInput = document.getElementById("uGR");
+  let ColInput = document.getElementById("uGC");
+  let BombInput = document.getElementById("uB");
+  let NumRows = (RowInput.value == "") ? RowInput.placeholder : RowInput.value;
+  let NumCols = (ColInput.value == "") ? ColInput.placeholder : ColInput.value;
+  let NumBombs = (BombInput.value == "") ? BombInput.placeholder : BombInput.value;
+
+  if(NumRows >= 2 && NumCols >= 2 && NumBombs >= 1 && NumBombs <= NumRows * NumCols - 1)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
 
 function CreateBoard(NumRows, NumCols, NumBombs)
 {
-  let WinScreen = document.getElementById("WinScreen");
-  WinScreen.style.display = "none";
-  let LoseScreen = document.getElementById("LoseScreen");
-  LoseScreen.style.display = "none";
-  let form = document.getElementById("msform");
-  form.style.display = "block";
+  if(GoodInputs())
+  {
+    let WinScreen = document.getElementById("WinScreen");
+    WinScreen.style.display = "none";
+    let LoseScreen = document.getElementById("LoseScreen");
+    LoseScreen.style.display = "none";
+    let form = document.getElementById("msform");
+    form.style.display = "block";
 
-  if(NumRows == undefined)
-  {
-    let RowInput = document.getElementById("uGR");
-    NumRows = (RowInput.value == "") ? RowInput.placeholder : RowInput.value;
+    if(NumRows == undefined)
+    {
+      let RowInput = document.getElementById("uGR");
+      NumRows = (RowInput.value == "") ? RowInput.placeholder : RowInput.value;
+    }
+    if(NumCols == undefined)
+    {
+      let ColInput = document.getElementById("uGC");
+      NumCols = (ColInput.value == "") ? ColInput.placeholder : ColInput.value;
+    }
+    if(NumBombs == undefined)
+    {
+      let BombInput = document.getElementById("uB");
+      NumBombs = (BombInput.value == "") ? BombInput.placeholder : BombInput.value;
+    }
+    place_grid(setup(parseInt(NumRows), parseInt(NumCols), parseInt(NumBombs)));
   }
-  if(NumCols == undefined)
-  {
-    let ColInput = document.getElementById("uGC");
-    NumCols = (ColInput.value == "") ? ColInput.placeholder : ColInput.value;
-  }
-  if(NumBombs == undefined)
-  {
-    let BombInput = document.getElementById("uB");
-    NumBombs = (BombInput.value == "") ? BombInput.placeholder : BombInput.value;
-  }
-  place_grid(setup(parseInt(NumRows), parseInt(NumCols), parseInt(NumBombs)));
 }
 
 
