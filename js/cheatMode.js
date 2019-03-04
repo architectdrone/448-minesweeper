29cac9e7775232c5ced9582c67ac05dc6e5cb053
Owen Mellema <owenmllm@gmail.com>
2019-03-04 17:00:47 -0600

    Removed stupid gorilla and added cheatMode.js

---
+++ js/cheatMode.js
@@ -0,0 +1,20 @@
+let cheatMode = false; //Whether or not we are currently in cheat mode.
+
+function onCheatModeButton()
+{
+    if (!cheatMode)
+    {
+        enableCheatMode();
+    }
+    else
+    {
+        disableCheatMode();
+    }
+}
+
+function enableCheatMode()
+{
+
+}
+
+function disableCheatMode()
