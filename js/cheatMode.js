1b255cd32a40a6d07e8b41710bbf0eacc5e88384
Owen Mellema <owenmllm@gmail.com>
2019-03-06 09:27:45 -0600

    Fix toggling of chet mode

--- js/cheatMode.js
+++ js/cheatMode.js
@@ -5,23 +5,27 @@ function onCheatModeButton()
     if (!cheatMode)
     {
         enableCheatMode();
+        cheatMode = true;
     }
     else
     {
         disableCheatMode();
+        cheatMode = false;
     }
 }
 
 function enableCheatMode()
 {
-    let cheatMode = document.getElementById("cheatBoard");
-    cheatMode.display = "block";
+    // let cheatMode = document.getElementById("cheatBoard");
+    // cheatMode.display = "block";
+    document.getElementById("cheatBoard").display = "block";
     document.getElementById("titleTag").innerHTML = "Cheat Mode Engaged";
 }
 
 function disableCheatMode()
 {
-    let cheatMode = document.getElementById("cheatBoard");
-    cheatMode.display = "none";
+    // let cheatMode = document.getElementById("cheatBoard");
+    // cheatMode.display = "none";
+    document.getElementById("cheatBoard").display = "none";
     document.getElementById("titleTag").innerHTML = "Minesweeper";
 }
