932399cd948931901b06274324c01913ab33a994
Owen Mellema <owenmllm@gmail.com>
2019-03-06 13:56:43 -0600

    Fixed cheatmode

--- js/cheatMode.js
+++ js/cheatMode.js
@@ -18,7 +18,8 @@ function enableCheatMode()
 {
     // let cheatMode = document.getElementById("cheatBoard");
     // cheatMode.display = "block";
-    document.getElementById("cheatBoard").display = "block";
+    document.getElementById("cheatBoard").style.display = "block";
+    document.getElementById("msBoard").style.display = "none";
     document.getElementById("titleTag").innerHTML = "Cheat Mode Engaged";
 }
 
@@ -26,6 +27,7 @@ function disableCheatMode()
 {
     // let cheatMode = document.getElementById("cheatBoard");
     // cheatMode.display = "none";
-    document.getElementById("cheatBoard").display = "none";
+    document.getElementById("cheatBoard").style.display = "none";
+    document.getElementById("msBoard").style.display = "block";
     document.getElementById("titleTag").innerHTML = "Minesweeper";
 }
