8d5fc1643096b5cbe6ad9a7a83632ed64cdac6c2
Gwen Liu <k579l290@ku.edu>
2019-03-17 22:51:23 -0500

    Added comments for time.js

--- js/time.js
+++ js/time.js
@@ -1,14 +1,21 @@
 //source: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_settimeout_cleartimeout2
-let c = 0;
-let t;
+//functions used for timer
+let c = 0; //numerical time value
+let t; //sets time delay for count
 let timer_on = false;
 
+/*
+Increments time by seconds
+*/
 function timedCount() {
   document.getElementById("Timer").innerHTML = c;
   c = c + 1;
   t = setTimeout(timedCount, 1000);
 }
 
+/*
+Starts timedCount()
+*/
 function startCount() {
   if (timer_on == false) {
     timer_on = true;
@@ -16,12 +23,18 @@ function startCount() {
   }
 }
 
+/*
+Stops timer
+*/
 function stopCount() {
   clearTimeout(t);
   timer_on = false;
 }
 
-function reset() {
+/*
+resets timer to 0
+*/
+function resetCount() {
   c = 0;
-  document.getElementById("Timer").innerHTML = 0;
+  document.getElementById("Timer").innerHTML = '0';
 }
